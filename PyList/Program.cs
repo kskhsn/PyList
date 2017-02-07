62345a01f6481d1b327c0a6edcf2025e674039ed
kei <kei@KH>
2017-02-07 21:15:58 +0900

    プロジェクト ファイルを追加します。

---
+++ PyList/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PyList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
